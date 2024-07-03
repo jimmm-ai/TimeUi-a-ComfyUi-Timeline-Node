961af7e09c090d95bbe9ce4e7a3ca470fc140778
corbin-hayden13 <haydenfcorbin@gmail.com>
2024-07-02 22:09:34 -0500

    stabliized html scaling for zooming in and out as best I could

--- js/managers/NodeManager.js
+++ js/managers/NodeManager.js
@@ -21,27 +21,29 @@ export const out = (message) => {
 };
 
 function get_position_style(ctx, widget_width, y, node_height, rowHeight) {
-  const MARGIN = 8;  // the margin around the html element
+  const MARGIN = 8;
 
 /**
  * Create a transform that deals with all the scrolling and zooming
- * This doesn't work as intended, at least not with specific margin px values
+ * This use of logarithms appears to be the most stable method to handle zooming
 */
   const elRect = ctx.canvas.getBoundingClientRect();
+  const scaleFactor = ctx.getTransform().a;  // Scale: Zoomed in=[0.1, 1), Normal=1, Zoomed out=(1, 10]
+  const scaledMargin = (1 + Math.log(MARGIN) / Math.log(2)) * (1 + Math.log10(MARGIN)) * (1 + (Math.log10(scaleFactor)));
   const transform = new DOMMatrix()
       .scaleSelf(elRect.width / ctx.canvas.width, elRect.height / ctx.canvas.height)
       .multiplySelf(ctx.getTransform())
-      .translateSelf(MARGIN, MARGIN + y);
+      .translateSelf(scaledMargin, scaledMargin + y);
 
   return {
       transformOrigin: '0 0',
-      transform: transform,
-      left: `${MARGIN}px`, 
-      top: `${MARGIN}px`,
+      transform: transform.toString(),
+      left: `${scaledMargin}px`, 
+      top: `${scaledMargin}px`,
       position: "absolute",
-      maxWidth: `${widget_width - MARGIN*4}px`,
+      maxWidth: `${widget_width - scaledMargin*4}px`,
       maxHeight: `${rowHeight}px`,    // we're assuming we have the whole height of the node
-      width: `${widget_width - MARGIN*4}px`,
+      width: `${widget_width - scaledMargin*4}px`,
       height: `${rowHeight}px`,
   }
 }
